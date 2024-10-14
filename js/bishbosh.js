632874ee8b20b119bafb241ff5e05014c5f786a1
Svitlana <rsv0011@gmail.com>
2024-10-14 21:10:40 +0200

    feat: render the game output on the page

--- js/bishbosh.js
+++ js/bishbosh.js
@@ -1,20 +1,22 @@
 const progressText = document.getElementById('progressText');
 const progressBar = document.getElementById('progressBar');
 const form = document.getElementById("bish-bosh-form");
+const gameResultList = document.getElementById('game-result-list');
 
 let progress = 0;
 const updateInterval = 50;
 let interval = null;  
 
-async function onFormSubmit(event) {
+ async function onFormSubmit(event) {
   event.preventDefault(); 
 
   const data = new FormData(event.target);
   
-  const bishNumber = data.get("bish-number-input");
-  const boshNumber = data.get("bosh-number-input");
+  const bishNumber = parseInt(data.get("bish-number-input"));
+  const boshNumber = parseInt(data.get("bosh-number-input"));
   const loopNumber = parseInt(data.get("loop-number-input"));
 
+  gameResultList.innerHTML = '';
   progress = 0;
   
   // Clear any existing intervals before starting a new one
@@ -24,7 +26,7 @@ async function onFormSubmit(event) {
 
   // Start the progress update 
   interval = await setInterval(() => updateProgress(loopNumber), updateInterval);
-  
+  renderGameResult(loopNumber, bishNumber, boshNumber);
 }
 
 function updateProgress(loopNumber) {
@@ -41,6 +43,25 @@ function updateProgress(loopNumber) {
   progressText.textContent = `${progress}`;
   progressBar.style.strokeDashoffset = 520 - (520 * adjustedProgress) / 100;
 
+}
+
+function renderGameResult(loopNumber, bishNumber, boshNumber) {
+  for (let i = 1; i <= loopNumber; i++) {
+    let result = '';
+    if (i % bishNumber === 0 && i % boshNumber === 0) {
+      result = 'Bish-Bosh';
+    } else if (i % bishNumber === 0) {
+      result = 'Bish';
+    } else if (i % boshNumber === 0) {
+      result = 'Bosh';
+    } else {
+      result = i;
+    }
+
+    const li = document.createElement('li');
+    li.textContent = result;
+    gameResultList.appendChild(li);
+  }
 }
 
 
